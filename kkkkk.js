a708ef89afe4812472b03fe622ba600ffc21971e
hassaanomar66 <165087843+hassaanomar66@users.noreply.github.com>
2024-04-01 05:56:43 +0300

    Create kkkkk.js

---
+++ kkkkk.js
@@ -0,0 +1,109 @@
+</section>
+<section id="best-offers-4-slider" class="s-block s-block--best-offers container overflow-hidden dm-text-main">
+<salla-products-slider source="selected" limit="16" autoplay source-value="[1837354832,435642643,1672282687,309041874,1021888812,1893822960,722282469,1515389133,1538417038,1737417742,1838669348,2082585057,2117728147,1768447365,913665876,1646522523]" display-all-url="https://salla.sa/lvainoad/latest-products" block-title="جديدنا" block-subTitle slider-id="slider-with-bg-4"></salla-products-slider>
+</section>
+<section class="s-block s-block--animated-ads  " style="--ad-speed:calc(60000ms / 4);--ad-x-pad:20px;">
+<div class="animated-ads-outer">
+<button type="button" class="animated-ads--content flex " data-coupon>
+<ul class="flex ad-ul custom-s" style="background-color:#000000;color:#ffffff;">
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+<li class="a-ad-text">
+<span>
+أهلا وسهلا بكم في لافانا للعود، كل عام وأنتم بخير
+</span>
+</li>
+</ul>
+</button>
+</div>
+</section>
+<section class="s-block s-block--features enhanced-features  anmt-up-down   container  " style="--f-width:80px;--f-height:80px;  --f-gap:12px;">
+<div class="features--title-outer custom-s section-main-title dm-text-main mb-2 text-center lg:text-center" style="color:#121212;">
